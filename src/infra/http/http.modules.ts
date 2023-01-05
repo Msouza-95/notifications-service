abe68cf5584bf1978278ad69c2fe825bee24abba
Msouza <msdsantos96@gmail.com>
2023-01-05 09:11:24 -0300

    create routes

--- src/infra/http/http.modules.ts
+++ src/infra/http/http.modules.ts
@@ -1,3 +1,8 @@
+import { CancelNotification } from "@application/use-cases/cancel-notification";
+import { CountRecipientNotification } from "@application/use-cases/count-recipient-notification";
+import { GetRecipientNotification } from "@application/use-cases/get-recipient-notification";
+import { ReadNotification } from "@application/use-cases/read-notification";
+import { UnreadNotification } from "@application/use-cases/unRead-notification";
 import { Module } from "@nestjs/common";
 import { SendNotification } from "src/application/use-cases/send-notification";
 import { DatabaseModule } from "../database/database.modules";
@@ -7,7 +12,14 @@ import { NotificationsController } from "./controllers/notifications.controller"
 @Module({
     imports:[ DatabaseModule],
     controllers: [ NotificationsController],
-    providers: [SendNotification]
+    providers: [
+        SendNotification,
+        CancelNotification,
+        CountRecipientNotification,
+        GetRecipientNotification,
+        ReadNotification,
+        UnreadNotification
+    ]
 })
 
 
